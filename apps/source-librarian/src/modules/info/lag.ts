bf2bc24c5d88399b5612ce4886c13cd188676c9e
Qther <qther@tuta.io>
2026-08-05 05:25:53 +0800

    feat: talk about --force-java-sampler for spark on Java 25 (#9)

--- apps/source-librarian/src/modules/info/lag.ts
+++ apps/source-librarian/src/modules/info/lag.ts
@@ -10,6 +10,7 @@ export class LagCommand extends InfoCommand {
   url = "https://spark.lucko.me/";
   lines = [
     "Spark is the best available tool for debugging lag. It provides more accurate reports as to what is causing the lag, but it doesn't show where in the world the lag is occuring. This is the one use-case that I'd recommend using Observable for.",
+    "If you are using Java 25 on Minecraft 1.21.1, you will need to add the `--force-java-sampler` flag when starting a profiler",
   ];
   fields = [
     {
